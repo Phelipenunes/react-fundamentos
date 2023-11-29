03ce49bcc2bb4c64a9ccb33519e7a491cde4fc2a
Phelipe <133427190+Phelipenunes@users.noreply.github.com>
2023-11-29 11:11:09 -0300

    formatação com o preter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 function App() {
-  return <h1>Olá react! 👍</h1>
+  // Comentario simples
+  /* Comentario composto */
+  return (
+    <>
+      <header>
+        <h1>Olá react! 👍</h1>
+        <hr />
+        <nav>
+          <a href="">home</a>
+          <a href="">Produtos</a>
+          <a href="">Contatos</a>
+        </nav>
+      </header>
+      {/* oi */}
+      <main>
+        <h2>oi</h2>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
