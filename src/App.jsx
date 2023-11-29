33729953eaa15392fc8ff88c3ae54819d7a0dfa7
Phelipe <133427190+Phelipenunes@users.noreply.github.com>
2023-11-29 11:17:37 -0300

    .
    .

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,43 @@ function App() {
     <>
       <header>
         <h1>Olá react! 👍</h1>
-        <hr />
+
         <nav>
           <a href="">home</a>
           <a href="">Produtos</a>
           <a href="">Contatos</a>
         </nav>
+        <hr />
       </header>
-      {/* oi */}
       <main>
-        <h2>oi</h2>
+        <section>
+          <h2>Conteúdo de aplicação</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt vitae
+            iusto qui perferendis sed repellendus odio recusandae quidem
+            perspiciatis natus! Placeat corporis debitis, ut earum odit
+            praesentium quas nemo saepe. Accusantium quaerat laboriosam porro
+            dolores ipsam nesciunt eaque repudiandae soluta doloribus sint
+            perferendis eius ducimus, delectus corporis voluptates itaque
+            doloremque recusandae repellendus esse. Earum, natus alias.
+            Doloremque itaque incidunt rem.
+          </p>
+
+          <article>
+            <h3>Artigo 1</h3>
+            <p>Conteúdo do artigo 1</p>
+          </article>
+
+          <article>
+            <h3>Artigo 2</h3>
+            <p>Conteúdo do artigo 2</p>
+          </article>
+
+          <article>
+            <h3>Artigo 3</h3>
+            <p>Conteúdo do artigo 3</p>
+          </article>
+        </section>
       </main>
     </>
   );
