f4635487e02276e0502f6e9d08365db638b88a03
Phelipe <133427190+Phelipenunes@users.noreply.github.com>
2023-11-29 10:01:10 -0300

    .
    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+/* Importação das libs ESSENCIAIS (React ReactDOM) */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+/* Importação de primeiro componente, arquivo em que carregamos os elementos da página (como se fosse um index/home)*/
 import App from './App.jsx'
-import './index.css'
+/* Inicialização dos recursos do React, manipulando a div#root existente no index.html */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
