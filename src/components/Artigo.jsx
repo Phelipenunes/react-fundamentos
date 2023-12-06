dee549a7f75b0e16b0e6ba6c5adb82382eaea45f
Phelipe <133427190+Phelipenunes@users.noreply.github.com>
2023-12-06 11:58:05 -0300

    exercicio
    .

--- src/components/Artigo.jsx
+++ src/components/Artigo.jsx
@@ -1,9 +1,19 @@
 import { StyledArtigo } from "../Style.js";
+import { useState } from "react";
 
 function Artigo({ titulo, categoria, preco }) {
+  const [fundo, setFundo] = useState("lightgray");
+
+  const mudarFundo = () => {
+    if (fundo === "lightgray") {
+      setFundo("yellow");
+    } else {
+      setFundo("lightgray");
+    }
+  };
   return (
     <>
-      <StyledArtigo>
+      <StyledArtigo onClick={mudarFundo} style={{ backgroundColor: fundo }}>
         <h3>{titulo}</h3>
         <p>
           <b>Curso: {categoria}</b>
